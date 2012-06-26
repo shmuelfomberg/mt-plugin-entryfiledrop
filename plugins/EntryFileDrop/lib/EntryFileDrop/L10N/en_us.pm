faac5b4ddb2cf22120750762d5217059e0ca09b8
Shmuel Fomberg <shmuelfomberg@gmail.com>
2012-06-26 19:59:37 +0900

    start L10N

---
+++ plugins/EntryFileDrop/lib/EntryFileDrop/L10N/en_us.pm
@@ -0,0 +1,9 @@
+package EntryFileDrop::L10N::en_us;
+
+use strict;
+use base 'EntryFileDrop::L10N';
+use vars qw( %Lexicon );
+
+%Lexicon = ();
+
+1;
---
+++ plugins/EntryFileDrop/lib/EntryFileDrop/L10N/ja.pm
@@ -0,0 +1,13 @@
+package EntryFileDrop::L10N::ja;
+
+use strict;
+use base 'EntryFileDrop::L10N::en_us';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+    'Enter the tags to be suggested' => '示唆するタグを入力ください',
+    'Manage Entry Asset Tags' => 'エントリのアセットタグを管理する',
+    'Add Tag \'[_1]\'' => '\'[_1]\'タグを追加',
+);
+
+1;
